bffe8d86f75ae1dab8e8d7bb9ad718ab28bef378
Lord Voldemort <murderthemuggles@yahoo.co.in>
2016-09-25 11:10:59 +0530

    Blank project

---
+++ app/src/main/java/kanj/apps/fancyassgallery/ui/FrontActivity.java
@@ -0,0 +1,15 @@
+package kanj.apps.fancyassgallery.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kanj.apps.fancyassgallery.R;
+
+public class FrontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front);
+    }
+}
